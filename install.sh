df6206aafcd2b393a1f72f2184cc8a2f8db300b7
Stefan Filkov <filkov@re3d.eu>
2026-02-17 15:16:23 +0200

    sddm

--- install.sh
+++ install.sh
@@ -28,6 +28,14 @@ echo "Setting up Fish..."
 # We run this command INSIDE fish using the -c flag
 fish -c 'curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher nickeb96/puffer-fish'
 
+# Run the installer script
+# Note: This clones into /usr/share/sddm/themes and edits /etc/sddm.conf
+if [ ! -d "/usr/share/sddm/themes/sddm-astronaut-theme" ]; then
+    echo "Installing Astronaut Theme..."
+    # We pipe to bash. The script usually handles sudo internally or asks for it.
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+fi
+
 # change default shell to fish
 sudo usermod -s /usr/bin/fish $USER
 
